5e420e0160c56033d13c7651bb62cedd61bb7dde
chasedrumfelt <chaserumfelt@gmail.com>
2026-01-10 17:36:38 -0500

    updated quote command so only admins can use it

--- tylerbot.py
+++ tylerbot.py
@@ -6,7 +6,7 @@ import re
 import string
 import logging
 import constants
-from yt_dlp import YoutubeDL
+from yt_dlp import YoutubeDL # type: ignore
 
 from fortnite_fetch import start_daily_shop_task
 from water_check import start_daily_water_check_task
@@ -14,7 +14,7 @@ from dice_roller import roll as dice_roll_command
 from familyguy_cutaway import search_youtube_video
 from quote_puller import get_random_quote
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv # type: ignore
 load_dotenv()
 
 intents = discord.Intents.default()
@@ -133,6 +133,9 @@ async def roll(ctx, *, dice: str):
 
 @bot.command(name="quote")
 async def quote(ctx):
+    if ctx.author.id not in constants.ADMIN_USER_IDS:
+        await ctx.send("Sorry dude, this one's ferda.")
+        return
     await get_random_quote(ctx, constants.QUOTE_CHANNEL)
 
 bot.run(constants.DISCORD_TOKEN)
